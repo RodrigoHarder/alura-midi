a30ed0dac74d747a74d446256fd8c543404801cf
Rodrigo Harder <114362538+RodrigoHarder@users.noreply.github.com>
2023-02-27 09:47:12 -0300

    Alterações no código JS

--- main.js
+++ main.js
@@ -98,8 +98,8 @@ function tocaSom(seletorAudio){
     
    const elemento = document.querySelector(seletorAudio);
     
-   if (elemento && elemento.localName === audio) {
-        elemento.play;
+   if (elemento && elemento.localName === 'audio') {
+        elemento.play();
      } else {
         alert('Elemento não encontrado')
      }
